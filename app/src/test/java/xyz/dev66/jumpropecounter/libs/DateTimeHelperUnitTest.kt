6cf33b2c69aacc2c38296a54f0d456448fb6d7ca
hwei <foreverfaint@gmail.com>
2019-11-29 18:01:30 +0800

    fix syntax warning about val and val usage

--- app/src/test/java/xyz/dev66/jumpropecounter/libs/DateTimeHelperUnitTest.kt
+++ app/src/test/java/xyz/dev66/jumpropecounter/libs/DateTimeHelperUnitTest.kt
@@ -11,40 +11,40 @@ class DateTimeHelperUnitTest {
     @ExperimentalTime
     @Test
     fun format1Second() {
-        var actual = formatTime(1000.toDuration(TimeUnit.MILLISECONDS))
-        var expected = "00:01.00"
+        val actual = formatTime(1000.toDuration(TimeUnit.MILLISECONDS))
+        val expected = "00:01.00"
         assertEquals(expected, actual)
     }
 
     @ExperimentalTime
     @Test
     fun format1Minute() {
-        var actual = formatTime(60000.toDuration(TimeUnit.MILLISECONDS))
-        var expected = "01:00.00"
+        val actual = formatTime(60000.toDuration(TimeUnit.MILLISECONDS))
+        val expected = "01:00.00"
         assertEquals(expected, actual)
     }
 
     @ExperimentalTime
     @Test
     fun format0Second() {
-        var actual = formatTime(0.toDuration(TimeUnit.MILLISECONDS))
-        var expected = "00:00.00"
+        val actual = formatTime(0.toDuration(TimeUnit.MILLISECONDS))
+        val expected = "00:00.00"
         assertEquals(expected, actual)
     }
 
     @ExperimentalTime
     @Test
     fun formatLessThan100Ms() {
-        var actual = formatTime(59.toDuration(TimeUnit.MILLISECONDS))
-        var expected = "00:00.05"
+        val actual = formatTime(59.toDuration(TimeUnit.MILLISECONDS))
+        val expected = "00:00.05"
         assertEquals(expected, actual)
     }
 
     @ExperimentalTime
     @Test
     fun formatLessThan1000Ms() {
-        var actual = formatTime(960.toDuration(TimeUnit.MILLISECONDS))
-        var expected = "00:00.96"
+        val actual = formatTime(960.toDuration(TimeUnit.MILLISECONDS))
+        val expected = "00:00.96"
         assertEquals(expected, actual)
     }
 }
